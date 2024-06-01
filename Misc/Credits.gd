df24cdaa00ede5fe97d54c836d178fc26b4b7f45
Sidnri <sindrihelgason@gmail.com>
2024-06-01 22:42:55 +0000

    finish decorations

---
+++ Misc/Credits.gd
@@ -0,0 +1,6 @@
+"""
+Droning sound effect by Pixabay
+French Forest Springtime sound effect by Pixabay
+Running Water Gentle Sound sound effect by Alex_Jauk
+Ambiance Wind Blowing Through Trees 01 sound effect by Traian1984
+"""
